0c7581997d50f3a5c5d0300dc25a039e566aa3c0
Robin Ruth <robin.ruth.rub@gmail.com>
2017-06-22 17:54:10 +0200

    make hompage responsive

--- js/main.js
+++ js/main.js
@@ -1,20 +1,53 @@
 
 
 
-
+var menuOpen = false;
 
 $(document).ready(function () {
 	$('section').load('./brainsnacks.html');
 	$('.logo').click(function() {
 		$('section').load('./brainsnacks.html');
 	});
+	$('.logoMobile').click(function() {
+		if (menuOpen) {
+			$('section').show();
+			$('.nav').hide();
+			$('li.active').removeClass('active');
+			$('section').load('./brainsnacks.html');
+		} else {
+			$('section').hide();
+			$('.nav').show();
+		}
+		menuOpen = !menuOpen;
+	})
 	$('li.about').click(function() {
+		$('li.active').removeClass('active');
+		if (menuOpen) {
+			$('section').show();
+			$('.nav').hide();
+		}
+		$('li.about').addClass('active');
+		menuOpen = false;
 		$('section').load('./about/about.html');
 	});
 	$('li.videos').click(function() {
+		$('li.active').removeClass('active');
+		if (menuOpen) {
+			$('section').show();
+			$('.nav').hide();
+		}
+		$('li.videos').addClass('active');
+		menuOpen = false;
 		$('section').load('./videos/videos.html');
 	});
 	$('li.contact').click(function() {
+		$('li.active').removeClass('active');
+		if (menuOpen) {
+			$('section').show();
+			$('.nav').hide();
+		}
+		$('li.contact').addClass('active');
+		menuOpen = false;
 		$('section').load('./contact/contact.html');
 	});
 });
